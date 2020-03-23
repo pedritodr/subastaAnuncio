153a8288839400cdec4422addd81c00887979c64
Pedro Luis <pedro@datalabcenter.com>
2020-03-23 17:06:36 -0500

    prueba paginado en anuncios

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -1220,6 +1220,8 @@ class Front extends CI_Controller
         $user_id = $this->session->userdata('user_id');
 
         if ($contador >= 6) {
+            var_dump("entro 6");
+            die();
             $config['per_page'] = '6';
         } else {
 
