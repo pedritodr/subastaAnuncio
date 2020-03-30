3b41c3b92f52dd9be9ba7f600ecc9d7693e73693
Pedro Luis <pedro@datalabcenter.com>
2020-03-30 16:57:20 -0500

    cambios en los rest

---
+++ application/controllers/Rest_membresia.php
@@ -0,0 +1,32 @@
+<?php
+require(APPPATH . "libraries/REST_Controller.php");
+require(APPPATH . "libraries/DatalabSecurity.php");
+//require(APPPATH . "libraries/proveedores/AlignetWallet.php");
+class Rest_membresia extends REST_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('Membresia_model', 'membresia');
+        $this->load->library(array('session'));
+        $this->load->helper("mabuya");
+
+        @session_start();
+        $this->load_language();
+        $this->init_form_validation();
+    }
+
+
+    public function listar_get() //econtrando usuario
+    {
+
+        $all_membresias = $this->membresia->get_all();
+        if ($all_membresias) {
+            $this->response(['status' => 200, 'all_membresias' => $all_membresias]);
+        } else {
+            $this->response(['status' => 500]);
+        }
+    }
+}
