ec72f5a2960c0921dd634e2c32402029aa0acd56
Pedro Luis <pedro@datalabcenter.com>
2020-04-14 13:58:01 -0500

    cambios en el boton de pago

--- application/config/routes.php
+++ application/config/routes.php
@@ -70,7 +70,7 @@ $route['prueba'] = 'front/prueba';
 $route['search_subasta_directa'] = 'front/buscar_subasta_directa';
 $route['search_subasta_inversa'] = 'front/buscar_subasta_inversa';
 $route['search_anuncios'] = 'front/buscar_anuncio';
-$route['payment'] = 'front/pago_exitoso';
+$route['payment/(:any)'] = 'front/pago_exitoso/$1';
 $route['transaccion_cancelada'] = 'front/pago_cancelada';
 
 require_once(BASEPATH . 'database/DB' . '.php');
