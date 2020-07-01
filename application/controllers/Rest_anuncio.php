077216883f8ac2e92110fafeb50190991f149d68
Pedro Luis <pedro@datalabcenter.com>
2020-06-30 19:05:13 -0500

    rest anuncio

--- application/controllers/Rest_anuncio.php
+++ application/controllers/Rest_anuncio.php
@@ -71,8 +71,8 @@ class Rest_anuncio extends REST_Controller
                 } else {
                     $item->corta = $item->descripcion;
                 }
-                if ($title > 19) {
-                    $item->titulo = substr($item->titulo, 0, 18) . "...";
+                if ($title > 20) {
+                    $item->titulo = substr($item->titulo, 0, 20) . "...";
                 } else {
                     $item->titulo = $item->titulo;
                 }
