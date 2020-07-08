4366f1d27cadc4394120acec651194ba9ec276bc
Pedro Luis <pedro@datalabcenter.com>
2020-07-08 17:28:53 -0500

    rest anuncio

--- application/controllers/Rest_anuncio.php
+++ application/controllers/Rest_anuncio.php
@@ -183,6 +183,7 @@ class Rest_anuncio extends REST_Controller
                 if ($obj_anuncio) {
                     $categoria_object = $this->categoria->get_by_subcate_id_object($obj_anuncio->subcate_id);
                     $obj_anuncio->subcategoria = $categoria_object;
+                    $obj_anuncio->fotos = $this->anuncio->get_all_fotos(['anuncio_id' => $anuncio_id]);
                 }
             } else {
                 $obj_anuncio = null;
