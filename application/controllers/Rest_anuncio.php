f131137a2a772f8b9f4553d9e98f46b1f4e2e26b
Pedro Luis <pedro@datalabcenter.com>
2020-07-15 14:54:46 -0500

    rest anuncio

--- application/controllers/Rest_anuncio.php
+++ application/controllers/Rest_anuncio.php
@@ -147,7 +147,7 @@ class Rest_anuncio extends REST_Controller
         $security_token = $this->input->post('security_token');
         $buscar = $this->input->post('buscar');
         $auth = $this->user->is_valid_auth($user_id, $security_token);
-        $comienza = $this->input->post('comienza_busqueda');
+        $comienza = $this->input->post('comienza');
         $ubicacion = $this->input->post('ubicacion');
         $ciudad = $this->input->post('ciudad');
         $this->response(['status' => 500, 'result' => $comienza]);
@@ -619,6 +619,64 @@ class Rest_anuncio extends REST_Controller
                 $this->response(['status' => 200, 'lista' => $ciudades, 'categorias' => $categorias]);
             } else {
                 $this->response(['status' => 404]);
+            }
+        } else {
+            $this->response(['status' => 500]);
+        }
+    }
+    public function buscar_anuncios_filter_post()
+    {
+
+        $user_id = $this->input->post('user_id');
+        $security_token = $this->input->post('security_token');
+        $buscar = $this->input->post('buscar');
+        $auth = $this->user->is_valid_auth($user_id, $security_token);
+        $comienza = $this->input->post('comienza');
+        $ubicacion = $this->input->post('ubicacion');
+        $ciudad = $this->input->post('ciudad');
+        $this->response(['status' => 500, 'result' => $comienza]);
+        $categoria = $this->input->post('categoria');
+        $subcategoria = $this->input->post('subcategoria');
+        $limite = 11;
+        $auth = $this->user->is_valid_auth($user_id, $security_token);
+        $infinito = false;
+        if ($comienza > 0) {
+            $infinito = true;
+        }
+
+        if ($auth) {
+            if ($ubicacion) {
+                $ciudad_obj = $this->pais->get_city($ubicacion);
+                if ($ciudad_obj) {
+                    $ciudad = $ciudad_obj->ciudad_id;
+                } else {
+                    $ciudad = 0;
+                }
+            }
+
+            $all_anuncios = $this->anuncio->search_by_name_pagination($limite, $comienza, $buscar, $ciudad, $categoria, $subcategoria);
+            foreach ($all_anuncios as $item) {
+                //     $item->titulo = str_replace("´", "", $item->titulo);
+                $title = strlen($item->titulo);
+                $long = strlen($item->descripcion);
+                if ($long > 99) {
+                    $item->corta = substr($item->descripcion, 0, 96) . "...";
+                } else {
+                    $item->corta = $item->descripcion;
+                }
+                if ($title > 19) {
+                    $item->titulo = substr($item->titulo, 0, 16) . "...";
+                } else {
+                    $item->titulo = $item->titulo;
+                }
+            }
+
+            if (count($all_anuncios) > 0) {
+
+
+                $this->response(['status' => 200, 'lista' => $all_anuncios]);
+            } else {
+                $this->response(['status' => 404, 'infinito' => $infinito]);
             }
         } else {
             $this->response(['status' => 500]);
