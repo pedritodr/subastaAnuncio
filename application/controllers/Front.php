d4b74dec52dc46b950dc0757ef1d5c8f56e61065
Pedro Luis <pedro@datalabcenter.com>
2020-07-28 17:21:23 -0500

    cambios en los filtros

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -1678,7 +1678,7 @@ class Front extends CI_Controller
 
         /*Obtiene el numero de registros a mostrar por pagina */
 
-        //    $config['uri_segment'] = 3;
+        $config['uri_segment'] = 3;
         /*Se personaliza la paginación para que se adapte a bootstrap*/
         $config['per_page'] = '8';
         $config['cur_tag_open'] = '<li class="active"><a href="#">';
@@ -1710,9 +1710,9 @@ class Front extends CI_Controller
 
         $this->pagination->initialize($config);
         $page = $this->uri->segment(3);
-        //     $offset = !$page ? 0 : $page;
+        $offset = !$page ? 0 : $page;
 
-        $all_anuncios = $this->anuncio->get_all_anuncios_with_pagination($config['per_page'], 0);
+        $all_anuncios = $this->anuncio->get_all_anuncios_with_pagination($config['per_page'], $offset);
         foreach ($all_anuncios as $item) {
             $long = strlen($item->descripcion);
 
