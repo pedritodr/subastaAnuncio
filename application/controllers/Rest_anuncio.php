5a3520a22dd635525ceccc1b25126e2e65b0879a
Pedro Luis <pedro@datalabcenter.com>
2020-09-16 14:04:53 -0500

    prueba

--- application/controllers/Rest_anuncio.php
+++ application/controllers/Rest_anuncio.php
@@ -77,8 +77,8 @@ class Rest_anuncio extends REST_Controller
                         // $item->corta = substr($item->descripcion, 0, 96) . "...";
                     }
                 }
-                if ($title > 19) {
-                    $item->titulo = substr($item->titulo, 0, 16) . "...";
+                if ($title > 18) {
+                    $item->titulo = substr($item->titulo, 0, 15) . "...";
                 } else {
                     $item->titulo = $item->titulo;
                 }
@@ -182,8 +182,8 @@ class Rest_anuncio extends REST_Controller
                 } else {
                     $item->corta = $item->descripcion;
                 } */
-                if ($title > 19) {
-                    $item->titulo = substr($item->titulo, 0, 16) . "...";
+                if ($title > 18) {
+                    $item->titulo = substr($item->titulo, 0, 15) . "...";
                 } else {
                     $item->titulo = $item->titulo;
                 }
